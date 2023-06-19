2c4541ac79cb992fd37afde827adbd77dee96749
Hamish MacDonald <elusivenode@gmail.com>
2023-06-19 22:38:14 +1000

    2015 q1 2 parts

---
+++ src/aoc_2015/aoc_2015_1.rs
@@ -0,0 +1,34 @@
+use std::fs;
+
+pub fn part_1() -> i32 {
+    let file_path = "/Users/hamish.macdonald/Dev/advent_of_code/puzzle_inputs/2015/1_1.txt";
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let mut floor: i32 = 0;
+    for c in contents.chars() {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => floor += 0,
+        }
+    }
+    floor
+}
+
+pub fn part_2() -> u32 {
+    let file_path = "/Users/hamish.macdonald/Dev/advent_of_code/puzzle_inputs/2015/1_1.txt";
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let mut floor: i32 = 0;
+    let mut idx: u32 = 0;
+    for c in contents.chars() {
+        idx += 1;
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => floor += 0,
+        }
+        if floor < 0 {
+            break;
+        }
+    }
+    idx
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use advent_of_code::aoc_2015;
+
 fn main() {
-    println!("Hello, world!");
+    let mut line = String::new();
+    println!("Retrieve the answers to Advent of Code questions");
+    println!("Enter the year and question number separated by a space (eg. 2015 1)");
+    std::io::stdin().read_line(&mut line).unwrap();
+    line.pop(); //remove trailing \n
+    let year_question: Vec<&str> = line.split(" ").collect();
+    let year_question: (&str, &str) = (year_question[0], year_question[1]);
+
+    match year_question {
+        ("2015", "1") => println!(
+            "2015 1 part 1:\n\tThe instructions take Santa to floor {}.\n\tThe character with position {} took Santa to the basement for the first time",
+            aoc_2015::aoc_2015_1::part_1(),
+            aoc_2015::aoc_2015_1::part_2()
+        ),
+        _ => println!("Not implemented"),
+    }
 }
